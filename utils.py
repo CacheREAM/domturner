822100eb800ecc074a6d4bfb791772ba6279a265
Jeremy <jeremypeevpe@gmail.com>
2024-09-16 15:32:57 -0700

    improved handling of game info

--- utils.py
+++ utils.py
@@ -12,7 +12,6 @@ def is_owner(ctx):
     return ctx.author.id in OWNER_IDS
 
 
-# Function to scrape website
 def scrape_website(url):
     try:
         response = requests.get(url)
@@ -39,14 +38,14 @@ def scrape_website(url):
         # Get the text from the striped-table inside the pane status div
         striped_table = soup.find('div', class_='pane status').find(
             'table', class_='striped-table')
-        table_text = ''
-        for row in striped_table.find_all('tr'):
-            cells = row.find_all('td')
-            for cell in cells:
-                table_text += cell.text.strip() + '\n'
+        status = striped_table.find_all('tr')[0].find_all('td')[1].text.strip()
+        address = striped_table.find_all('tr')[1].find_all('td')[
+            1].text.strip()
+        next_turn = striped_table.find_all(
+            'tr')[2].find_all('td')[1].text.strip()
         # Get the game name
         game_name = soup.find('h1').text.strip()
-        return scraped_data, table_text, game_name, nations_data
+        return scraped_data, status, address, next_turn, game_name, nations_data
     except Exception as e:
         logger.error(f'Error: {e}')
-        return None, None, None, None
+        return None, None, None, None, None, None
