830d977dcd44e50a3712422285f2f468a9c28138
Jeremy <jeremypeevpe@gmail.com>
2024-10-22 14:01:09 -0700

    test fix of user overwrite

--- utils.py
+++ utils.py
@@ -43,7 +43,6 @@ def scrape_website(url, existing_nations_data=None):
             'td', class_='nation-name wide-column')
         scraped_data = []
         nations_data = {}
-        nation_id = 1
         for cell in nation_name_cells:
             nation_name = cell.find(
                 'b').text.strip() if cell.find('b') else None
@@ -51,12 +50,20 @@ def scrape_website(url, existing_nations_data=None):
                                                 'submitted', 'unsubmitted', 'unfinished', 'computer', 'dead'])
             status = [cell.text.strip() for cell in status_cells]
             scraped_data.append((nation_name, status))
-            existing_nation_data = existing_nations_data.get(
-                str(nation_id), {}) if existing_nations_data else {}
+            # Get the user from existing_nations_data
+            if existing_nations_data:
+                existing_nation = next((nation for nation in existing_nations_data.values(
+                ) if nation.get('name') == nation_name), None)
+                if existing_nation:
+                    user = existing_nation.get('user')
+                else:
+                    user = None
+            else:
+                user = None
             nations_data[str(nation_id)] = {
                 'name': nation_name,
                 'status': status[0] if status else None,
-                'user': existing_nation_data.get('user')
+                'user': user
             }
             nation_id += 1
         # Get the text from the striped-table inside the pane status div
