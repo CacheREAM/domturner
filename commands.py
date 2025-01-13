3a030fec8e0a449c264c0a0ac9a4a37e9c67ae4d
Jeremy <jeremypeevpe@gmail.com>
2025-01-12 17:45:27 -0800

    remake of counting unready users in each game with turns command

--- commands.py
+++ commands.py
@@ -298,27 +298,25 @@ async def turns(ctx, user: discord.Member = None):
 
     output = ""
     for channel_id, channel_data in channels.items():
-        game_unready_count = 0
-        user_nations = []
+        unfinished_count = 0
+        has_nation = False
         for nation_id, nation_info in channel_data['nations'].items():
             if nation_info.get('user') == str(user_id):
+                has_nation = True
+                if nation_info['status'] in ['unsubmitted', 'unfinished', '-', 'Turn unfinished']:
+                    unfinished_count += 1
                 if channel_data['options']['emoji_mode']:
                     status = f"{EMOJIS.get(nation_info['status'], '')} {
                         nation_info['status']}"
                 else:
                     status = nation_info['status']
-                user_nations.append((nation_id, nation_info))
-        if user_nations:
-            output += f"{channel_data['game_name']}\n"
-            for nation_id, nation_info in user_nations:
-                output += f"  - Nation {nation_id} ({nation_info['name']}): {
-                    status}\n"
-                output += f"  Turn: {channel_data.get('turn', 'N/A')}, Next Turn: {
-                    channel_data.get('next_turn', 'N/A')}\n"
-                if nation_info['status'] in ['unsubmitted', 'unfinished', '-', 'Turn unfinished']:
-                    game_unready_count += 1
-            output += f"  Number of unready players in this game: {
-                game_unready_count}\n\n"
+                output += f"{channel_data['game_name']} - Nation {
+                    nation_id} ({nation_info['name']}): {status}\n"
+                output += f"Turn: {channel_data.get('turn', 'N/A')}, Next Turn: {
+                    channel_data.get('next_turn', 'N/A')}\n\n"
+        if has_nation:
+            output += f"Total unfinished nations for {
+                channel_data['game_name']}: {unfinished_count}\n\n"
 
     if output:
         await ctx.send(output)
